08f8089f8e8aea1c86b42d7c800e0ee6103b5dee
Kaan Caliskan <kcaliskan@lcs.on.ca>
2024-11-12 14:53:09 -0500

    Initial Commit

---
+++ TradingCards/TradingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardsApp.swift
+//  TradingCards
+//
+//  Created by Ahmet Kaan Caliskan on 2024-11-12.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
