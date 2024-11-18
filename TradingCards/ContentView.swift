9a62c7f2838243fdddaddb59062308cb6463c417
Kaan Caliskan <kcaliskan@lcs.on.ca>
2024-11-17 19:08:33 -0500

    template

--- TradingCards/ContentView.swift
+++ TradingCards/ContentView.swift
@@ -9,13 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .center, spacing: 0) {
+           
+            HStack { //Player Name Top Title
+                      Text("Player Name")
+                        .font(.system(size: 36, weight: .bold))
+                  } .frame(height: 50)
+            
+            Spacer()
+               
+            HStack { //Image and etc.
+                ZStack{
+                    Text("Player Image")
+                        .padding(15)
+                } .border(Color.green, width: 15)
+                  }.frame(height: 500)
+                .padding(.top, -150)
+            
+            
+            HStack { //Stats
+                    VStack{
+                        Text("Season Stats:")
+                        Text("PPG:")
+                        Text("RPG:")
+                        Text("AG:")
+                        Text("FG%:")
+                    }.padding(50)
+                Spacer()
+                    VStack{
+                        Text("Position")
+                        Text("Jersey Number")
+                        Text("Height")
+                        Text("Weight")
+                        Text("Age")
+                    }.padding(50)
+                     
+            }.padding(.top, 0)
         }
-        .padding()
+      
     }
 }
 
