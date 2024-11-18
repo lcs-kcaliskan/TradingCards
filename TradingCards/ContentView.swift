50833a876eabafe240d4fc07abec04cf1c507f5f
Kaan Caliskan <kcaliskan@lcs.on.ca>
2024-11-17 19:34:56 -0500

    The middle Hstack is basically done

--- TradingCards/ContentView.swift
+++ TradingCards/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
         VStack(alignment: .center, spacing: 0) {
            
             HStack { //Player Name Top Title
-                      Text("Player Name")
+                      Text("Jaylen Brown")
                         .font(.system(size: 36, weight: .bold))
                   } .frame(height: 50)
             
@@ -20,8 +20,18 @@ struct ContentView: View {
                
             HStack { //Image and etc.
                 ZStack{
-                    Text("Player Image")
-                        .padding(15)
+                    Image("JaylenBrown")
+                        .resizable()
+                        .scaledToFit()
+                    HStack{
+                        Image("CelticsLogo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                            .offset(x: 130, y: 200)
+                        
+                    }
+                .padding(15)
                 } .border(Color.green, width: 15)
                   }.frame(height: 500)
                 .padding(.top, -150)
@@ -30,19 +40,29 @@ struct ContentView: View {
             HStack { //Stats
                     VStack{
                         Text("Season Stats:")
+                            .font(.system(size: 20, weight: .semibold))
                         Text("PPG:")
+                            .font(.system(size: 20, weight: .semibold))
                         Text("RPG:")
+                            .font(.system(size: 20, weight: .semibold))
                         Text("AG:")
+                            .font(.system(size: 20, weight: .semibold))
                         Text("FG%:")
-                    }.padding(50)
+                            .font(.system(size: 20, weight: .semibold))
+                    }.padding(20)
                 Spacer()
                     VStack{
                         Text("Position")
+                            .font(.system(size: 20, weight: .semibold))
                         Text("Jersey Number")
+                            .font(.system(size: 20, weight: .semibold))
                         Text("Height")
+                            .font(.system(size: 20, weight: .semibold))
                         Text("Weight")
+                            .font(.system(size: 20, weight: .semibold))
                         Text("Age")
-                    }.padding(50)
+                            .font(.system(size: 20, weight: .semibold))
+                    }.padding(45)
                      
             }.padding(.top, 0)
         }
