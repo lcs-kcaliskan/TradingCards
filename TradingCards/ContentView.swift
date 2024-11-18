2736ada6708a86cad8b7565ba9822a092babc7cf
Kaan Caliskan <kcaliskan@lcs.on.ca>
2024-11-17 22:06:21 -0500

    Final draft for Sunday, all is done except the decimals of the doubles and I want to change the bordercolor to be interchangeable as a variable

--- TradingCards/ContentView.swift
+++ TradingCards/ContentView.swift
@@ -9,95 +9,119 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-       
-        VStack(alignment: .center, spacing: 0) {
-           
-            HStack { //Player Name Top Title
-                      Text("Jaylen Brown")
+        PlayerCard(playerName: "Jaylen Brown", playerImage: "JaylenBrown", playerTeamLogoImage: "CelticsLogo", pointsPerGame: 25.6, reboundsPerGame: 7.1, assistsPerGame: 4.0, fieldGoalPercentage: 43.0, playerPosition: "SG", playerJerseyNumber: 7, playerHeight: "6'6", playerWeight: 223, playerAge: 28)
+        
+    }
+}
+    #Preview {
+        ContentView()
+    }
+
+
+    
+    
+    struct PlayerCard: View {
+        let playerName: String
+        let playerImage: String
+        let playerTeamLogoImage: String
+        let pointsPerGame: Double
+        let reboundsPerGame: Double
+        let assistsPerGame: Double
+        let fieldGoalPercentage: Double
+        let playerPosition: String
+        let playerJerseyNumber: Int
+        let playerHeight: String
+        let playerWeight: Int
+        let playerAge: Int
+        
+        var body: some View {
+            VStack(alignment: .center, spacing: 0) {
+                
+                HStack { //Player Name Top Title
+                    Text(playerName)
                         .font(.system(size: 36, weight: .bold))
-                  } .frame(height: 50)
-            
-            Spacer()
-               
-            HStack { //Image and etc.
-                ZStack{
-                    Image("JaylenBrown")
-                        .resizable()
-                        .scaledToFit()
-                    HStack{
-                        Image("CelticsLogo")
+                } .frame(height: 50)
+                
+                Spacer()
+                
+                HStack { //Image and etc.
+                    ZStack{
+                        Image(playerImage)
                             .resizable()
                             .scaledToFit()
-                            .frame(width: 100, height: 100)
-                            .offset(x: 130, y: 200)
-                        
-                    }
-                .padding(15)
-                } .border(Color.green, width: 15)
-                  }.frame(height: 500)
-                .padding(.top, -150)
-           
-           
-            
-            HStack { //Stats
+                        HStack{
+                            Image(playerTeamLogoImage)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100, height: 100)
+                                .offset(x: 130, y: 200)
+                            
+                        }
+                        .padding(15)
+                    } .border(Color.black , width: 15)
+                }.frame(height: 500)
+                    .padding(.top, -150)
+                
+                
+                
+                HStack { //Stats
                     VStack{
                         Text("Season Stats:")
                             .font(.system(size: 20, weight: .semibold))
                             .foregroundColor(.white)
-                        Text("25.6 PPG")
+                        Text("\(pointsPerGame) PPG")
                             .foregroundColor(.white)
                             .font(.system(size: 20, weight: .semibold))
-                        Text("7.1 RPG")
+                        Text("\(reboundsPerGame)RPG")
                             .foregroundColor(.white)
                             .font(.system(size: 20, weight: .semibold))
-                        Text("4.0 AG")
+                        Text("\(assistsPerGame)APG")
                             .foregroundColor(.white)
                             .font(.system(size: 20, weight: .semibold))
-                        Text("43.0% FG")
+                        Text("\(fieldGoalPercentage)%FG")
                             .foregroundColor(.white)
                             .font(.system(size: 20, weight: .semibold))
                     }.padding(20)
-                Spacer()
+                    Spacer()
                     VStack{
-                        Text("SG")
+                        Text(playerPosition)
                             .foregroundColor(.white)
                             .font(.system(size: 20, weight: .bold))
-                        Text("#7")
+                        Text("#\(playerJerseyNumber)")
                             .foregroundColor(.white)
                             .font(.system(size: 20, weight: .bold))
-                        Text("6'6")
+                        Text(playerHeight)
                             .foregroundColor(.white)
                             .font(.system(size: 20, weight: .bold))
-                        Text("223 lbs")
+                        Text("\(playerWeight)lbs")
                             .foregroundColor(.white)
                             .font(.system(size: 20, weight: .bold))
-                        Text("28")
+                        Text("\(playerAge)")
                             .foregroundColor(.white)
                             .font(.system(size: 20, weight: .bold))
                     }.padding(45)
                     
-                     
-            }.padding(.top, 0)
-                .background(ZStack {
-                    Image("StatsBackground")
-                        .resizable()
-                        .scaledToFill()
-                        .ignoresSafeArea()
-                        .frame(width: 200, height: 210)
-                        .offset(x: -28, y: 0)
-                       
-                })
+                    
+                }.padding(.top, 0)
+                    .background(ZStack {
+                        Image("StatsBackground")
+                            .resizable()
+                            .scaledToFill()
+                            .ignoresSafeArea()
+                            .frame(width: 200, height: 210)
+                            .offset(x: -28, y: 0)
+                        
+                    })
+            }
+            .background(ZStack {
+                Image("backgroundImage")
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
+            })
+            
+            
+            
         }
-        .background(ZStack {
-            Image("backgroundImage")
-                .resizable()
-                .scaledToFill()
-                .ignoresSafeArea()
-        })
     }
-        
-}
 
-#Preview {
-    ContentView()
-}
